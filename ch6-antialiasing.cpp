09c9c7c7f73b31e968f0dcdd27182962ebceeb00
Joshua O'Reilly <me@joshuaoreilly.com>
2022-12-20 11:05:55 -0500

    Chapter 6: Antialiasing

---
+++ ch6-antialiasing.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include "sphere.h"
+#include "hitable_list.h"
+#include "float.h"
+#include "camera.h"
+
+
+vec3 color(const ray& r, hitable *world) {
+    hit_record rec;
+    if (world->hit(r, 0.0, MAXFLOAT, rec)) {
+        return 0.5 * vec3(rec.normal.x()+1, rec.normal.y()+1, rec.normal.z()+1);
+    }
+    else {
+        vec3 unit_direction = unit_vector(r.direction());
+        float t = 0.5 * (unit_direction.y() + 1.0);
+        return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+    }
+}
+
+int main() {
+    int nx = 200;
+    int ny = 100;
+    int ns = 100; // number of samples
+    std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+    hitable *list[2];
+    list[0] = new sphere(vec3(0, 0, -1), 0.5);
+    // really big sphere that'll look like ground when only looking at the top of it
+    list[1] = new sphere(vec3(0, -100.5, -1), 100);
+    hitable *world = new hitable_list(list, 2);
+    camera cam;
+    for (int j = ny-1; j >= 0; j--) {
+        for (int i = 0; i < nx; i++) {
+            vec3 col(0, 0, 0);
+            for (int s = 0; s < ns; s++) {
+                // drand48 gives value 0 <= x < 1, so we're sampling between current pixel and next in x and y
+                float u = float(i + drand48()) / float(nx);
+                float v = float(j + drand48()) / float(ny);
+                ray r = cam.get_ray(u, v);
+                // doesn't actually do anything, since the contact point is now
+                // contained in hit_record; maybe being kept around for future chapters?
+                vec3 p = r.point_at_parameter(2.0);
+                col += color(r, world);
+            }
+            col /= float(ns); // average (divide by number of samples)
+            int ir = int(255.99*col[0]);
+            int ig = int(255.99*col[1]);
+            int ib = int(255.99*col[2]);
+            std:: cout << ir << " " << ig << " " << ib << "\n";
+        }
+    }
+}
