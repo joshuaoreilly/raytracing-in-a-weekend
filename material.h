b1083424c4555a58ec59c6cd1c52e66123ca787a
Joshua O'Reilly <me@joshuaoreilly.com>
2023-02-08 17:21:49 +0100

    Chapter 9: Dielectrics
    This chapter definitely threw a lot of math around in the code, and very
    little in terms of explanation.
    Can't say I understand it compared to the other chapters.

--- material.h
+++ material.h
@@ -10,6 +10,34 @@ vec3 reflect(const vec3& v, const vec3& n) {
     return v - 2 * dot(v, n) * n;
 }
 
+bool refract(const vec3& v, const vec3& n, float ni_over_nt, vec3& refracted) {
+    /*
+    refraction will not occur if:
+    1. travelling from more to less dense medium
+    2. angle of incidence is greater than the critical angle
+    this results in the angle of refraction being 90 degrees or greater, so all light is reflected back.
+    Honestly, not super sure how this code works, seems like an optimized version of solving snell's law?
+    ni_over_nt = incident refraction index over transmitted refraction index?
+    */
+    vec3 uv = unit_vector(v);
+    float dt = dot(uv, n);
+    float discriminant = 1.0 - ni_over_nt * (1.0 - dt * dt);
+    if (discriminant > 0) {
+        refracted = ni_over_nt * (uv - n * dt) - n * sqrt(discriminant);
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+float schlick(float cosine, float ref_idx) {
+    // magic polynomial approximation of reflectivity varying with angle
+    float r0 = (1 - ref_idx) / (1 + ref_idx);
+    r0 = r0 * r0;
+    return r0 + (1 - r0) * pow((1 - cosine), 5);
+}
+
 vec3 random_in_unit_sphere() {
     /*
     randomly sample points in a unit cube whose base touches the contact point
@@ -57,6 +85,48 @@ class metal : public material {
 
         vec3 albedo;
         float fuzz;
+};
+
+class dielectric: public material {
+    public:
+        dielectric(float ri) : ref_idx(ri) {}
+
+        virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const {
+            vec3 outward_normal;
+            vec3 reflected = reflect(r_in.direction(), rec.normal);
+            float ni_over_nt;
+            attenuation = vec3(1.0, 1.0, 1.0);
+            vec3 refracted;
+            float reflect_prob;
+            float cosine;
+
+            if (dot(r_in.direction(), rec.normal) > 0) {
+                outward_normal = -rec.normal;
+                ni_over_nt = ref_idx;
+                cosine = ref_idx * dot(r_in.direction(), rec.normal) / r_in.direction().length();
+            }
+            else {
+                outward_normal = rec.normal;
+                ni_over_nt = 1.0 / ref_idx;
+                cosine = -dot(r_in.direction(), rec.normal) / r_in.direction().length();
+            }
+            if (refract(r_in.direction(), outward_normal, ni_over_nt, refracted)) {
+                reflect_prob = schlick(cosine, ref_idx);
+            }
+            else {
+                scattered = ray(rec.p, reflected);
+                reflect_prob = 1.0;
+            }
+            if (drand48() < reflect_prob) {
+                scattered = ray(rec.p, reflected);
+            }
+            else {
+                scattered = ray(rec.p, refracted);
+            }
+            return true;
+        }
+
+        float ref_idx;
 };
 
 #endif
