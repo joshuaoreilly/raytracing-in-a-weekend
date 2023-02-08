521dd586f9ee128e6fefed24c6c9f61573aa7c25
Joshua O'Reilly <me@joshuaoreilly.com>
2023-02-08 18:08:37 +0100

    Chapter 11: Defocus blur
    I get the intuition for depth of field and how we're mimicking it,
    but again, the implementation in code is a little magic.

--- camera.h
+++ camera.h
@@ -3,11 +3,24 @@
 
 #include "ray.h"
 
+vec3 random_in_unit_disk() {
+    /*
+    randomly sample points in a unit square, then reject them if they're not
+    in the unit circle contained in the unit square.
+    */
+    vec3 p;
+    do {
+        p = 2.0 * vec3(drand48(), drand48(), 0) - vec3(1, 1, 0);
+    } while (p.squared_length() >= 1.0);
+    return p;
+}
+
 class camera {
     public:
-        camera(vec3 lookfrom, vec3 lookat, vec3 vup, float vfov, float aspect) {
+        camera(vec3 lookfrom, vec3 lookat, vec3 vup, float vfov, float aspect, float aperture, float focus_dist) {
             // vfov = vertical field of view from top-to-bottom in degrees (not just from camera normal to top)
             // aspect = aspect ratio (between vertical and horizontal)
+            lens_radius = aperture / 2;
             vec3 u, v, w;
             float theta = vfov * M_PI / 180;
             float half_height = tan(theta / 2);
@@ -18,20 +31,24 @@ class camera {
             v = cross(w, u); // gives vector along camera y axis
             //lower_left_corner = vec3(-half_width, -half_height, -1.0);
             // all vectors now that we're no longer nicely aligned with our world frame anymore
-            lower_left_corner = origin - half_width * u - half_height * v - w;
-            horizontal = 2 * half_width * u;
-            vertical = 2 * half_height * v;
+            lower_left_corner = origin - half_width * focus_dist * u - half_height * focus_dist * v - focus_dist * w;
+            horizontal = 2 * half_width * focus_dist * u;
+            vertical = 2 * half_height * focus_dist * v;
         }
         
         ray get_ray(float s, float t) {
             // little change of notation since u and v already used
-            return ray(origin, lower_left_corner + s*horizontal + t*vertical - origin);
+            vec3 rd = lens_radius * random_in_unit_disk();
+            vec3 offset = u * rd.x() + v * rd.y();
+            return ray(origin + offset, lower_left_corner + s*horizontal + t*vertical - origin - offset);
         }
 
         vec3 origin;
         vec3 lower_left_corner;
         vec3 horizontal;
         vec3 vertical;
+        vec3 u, v, w;
+        float lens_radius;
 };
 
 #endif
