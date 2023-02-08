0f8934e5b3cc5fdbf0edf6fd490651397975f23b
Joshua O'Reilly <me@joshuaoreilly.com>
2023-02-08 17:42:48 +0100

    Chapter 10: Positionable camera

--- camera.h
+++ camera.h
@@ -5,15 +5,27 @@
 
 class camera {
     public:
-        camera() {
-            lower_left_corner = vec3(-2.0, -1.0, -1.0);
-            horizontal = vec3(4.0, 0.0, 0.0);
-            vertical = vec3(0.0, 2.0, 0.0);
-            origin = vec3(0.0, 0.0, 0.0);
+        camera(vec3 lookfrom, vec3 lookat, vec3 vup, float vfov, float aspect) {
+            // vfov = vertical field of view from top-to-bottom in degrees (not just from camera normal to top)
+            // aspect = aspect ratio (between vertical and horizontal)
+            vec3 u, v, w;
+            float theta = vfov * M_PI / 180;
+            float half_height = tan(theta / 2);
+            float half_width = aspect * half_height;
+            origin = lookfrom;
+            w = unit_vector(lookfrom - lookat); // w points away from where the camera is pointing
+            u = unit_vector(cross(vup, w)); // cross of vup (true up in scene) and w gives vector along camera x axis
+            v = cross(w, u); // gives vector along camera y axis
+            //lower_left_corner = vec3(-half_width, -half_height, -1.0);
+            // all vectors now that we're no longer nicely aligned with our world frame anymore
+            lower_left_corner = origin - half_width * u - half_height * v - w;
+            horizontal = 2 * half_width * u;
+            vertical = 2 * half_height * v;
         }
-
-        ray get_ray(float u, float v) {
-            return ray(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+        
+        ray get_ray(float s, float t) {
+            // little change of notation since u and v already used
+            return ray(origin, lower_left_corner + s*horizontal + t*vertical - origin);
         }
 
         vec3 origin;
